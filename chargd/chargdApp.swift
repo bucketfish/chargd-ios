b921d3f5a112f4be8060c26ca76590774fb2cbf2
bucketfishy <bucketfishy@gmail.com>
2023-03-13 17:22:24 +0800

    Initial Commit

---
+++ chargd/chargdApp.swift
@@ -0,0 +1,17 @@
+//
+//  chargdApp.swift
+//  chargd
+//
+//  Created by Tongyu Jiang on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct chargdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
