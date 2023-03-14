0b27fe8b93a34b9b2bf0d54e0cdd5b969dc1a698
bucketfishy <bucketfishy@gmail.com>
2023-03-14 21:14:54 +0800

    fixed bug where plugging out caption screen says plug in

--- chargd/Caption.swift
+++ chargd/Caption.swift
@@ -45,6 +45,9 @@ struct Caption: View {
             
             
         }.padding([.leading, .trailing])
+            .onAppear {
+                is_plugin = getPluggedState()
+            }
         
        
         
